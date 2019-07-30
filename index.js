bef356f3c5053864a825c633297085ab21117534
Andrew P Maney <amaney@usc.edu>
2019-07-30 11:37:21 -0700

    Heroku port

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ app.use(router);
 app.use(errorHandler);
 
 /* Now start our app */
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log('Snips server running on port 5000');
 });
