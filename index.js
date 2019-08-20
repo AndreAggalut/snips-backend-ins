354f9584aba8b27838fd5901f0edfe7f1fe4a893
Scott Roberts <s.e.roberts0@gmail.com>
2019-08-20 11:28:05 -0700

    Add CORS

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const logger = require('./middleware/logger');
 const router = require('./middleware/routes');
 const errorHandler = require('./middleware/errorHandler');
@@ -6,6 +7,8 @@ const errorHandler = require('./middleware/errorHandler');
 const app = express();
 
 /* Middleware */
+// TODO: Add CORS with all origins for testing
+app.use(cors());
 app.use(express.json()); // parses requests with JSON payloads
 app.use(logger);
 app.use(router);
