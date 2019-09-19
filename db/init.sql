ad84258f6b79691eb7998da621a2e0f08adec026
Andrew P Maney <amaney@usc.edu>
2019-09-19 12:55:18 -0700

    Adds models

--- db/init.sql
+++ db/init.sql
@@ -1,14 +1,24 @@
 DROP TABLE IF EXISTS snippet;
+DROP TABLE IF EXISTS author;
+--
+CREATE TABLE author (name TEXT PRIMARY KEY, password TEXT);
 CREATE TABLE snippet (
   id SERIAL PRIMARY KEY,
   code TEXT,
   title TEXT,
   description TEXT,
   favorites INT DEFAULT 0,
-  author TEXT,
+  -- establish snippet-author relationship
+  author TEXT REFERENCES author,
   language TEXT
 );
--- Seed snippets with data
+-- Seed author data
+INSERT INTO
+  author (name, password)
+VALUES
+  ('Dandy', 'dean123'),
+  ('Scott', 'password');
+-- Seed snippet data
 INSERT INTO
   snippet (code, title, description, language, author)
 VALUES
---
+++ models/Author.model.js
@@ -0,0 +1,26 @@
+const db = require('../db');
+const ErrorWithHttpStatus = require('../utils/ErrorWithHttpStatus');
+
+exports.insert = ({ password, name }) => {
+  try {
+    if (!password || !name) throw new ErrorWithHttpStatus('Missing props', 400);
+
+    return db.query('INSERT INTO author (name, password) VALUES ($1,$2)', [
+      name,
+      password,
+    ]);
+  } catch (err) {
+    if (err instanceof ErrorWithHttpStatus) throw err;
+    else throw new ErrorWithHttpStatus('Database error');
+  }
+};
+
+exports.select = async name => {
+  try {
+    const sql = `SELECT * FROM author WHERE name = $1`;
+    const result = await db.query(sql, name);
+    return result.rows[0];
+  } catch (err) {
+    throw new ErrorWithHttpStatus('Database error');
+  }
+};
